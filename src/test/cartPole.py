7018c5fe2eda4953d3081cd38e64412a3a1a6ee0
millscb <christopher.b.mills@gmail.com>
2025-09-16 08:36:28 -0500

    setup test for cart

--- src/test/cartPole.py
+++ src/test/cartPole.py
@@ -2,4 +2,12 @@ import gymnasium as gym
 
 from stable_baselines3 import PPO
 
+env = gym.make("CartPole-v1")
+obs = env.reset()
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10000)
 
+print(obs)
+
+
+obs = env.reset()
