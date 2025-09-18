b506c94622ccf904e4e9cf545ecde967022e1795
millscb <christopher.b.mills@gmail.com>
2025-09-18 09:31:06 -0500

    reorg folders and files

--- src/test/nupic_smoke_test.py
+++
@@ -1,6 +0,0 @@
-try:
-    import nupic
-    print("NuPIC import OK, version:", getattr(nupic, "__version__", "unknown"))
-except Exception as e:
-    print("NuPIC import failed:", repr(e))
-    raise
